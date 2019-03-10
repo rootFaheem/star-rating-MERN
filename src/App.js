0c8d561aed47113703e541cd9cd48a2b56f74100
Faheem <faheemtech1@gmail.com>
2019-03-11 00:06:51 +0530

    modal opens on click and now POST req has to made

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       this.rateData = (
         <Rating
           className="rating"
-          stop={10}
+          stop={5}
           emptySymbol={[
             "fa fa-star-o fa-2x medium",
             "fa fa-star-o fa-2x medium",
@@ -40,26 +40,42 @@ class App extends Component {
     } else this.rateData = null;
 
     return (
-      <div className="App">
-        <h1>Let's Rate it !</h1>
-        <br />
-
-        <div className="ratingbox">
-          <button
-            style={{ marginTop: "40px" }}
-            className="btn btn-success btn-lg"
-            onClick={() => this.setState({ show: true })}
-          >
-            {" "}
-            Rate Now
-          </button>
+      <div>
+        <div className="App">
+          <h1>Let's Rate it !</h1>
           <br />
 
-          {this.rateData}
-          <br />
-          <br />
-          <br />
-          <br />
+          <div className="ratingbox">
+            <div className="container">
+              <button
+                type="button"
+                className="btn btn-success btn-lg rating"
+                id="myBtn"
+                onClick={() => this.setState({ show: true })}
+              >
+                Login
+              </button>
+
+              <div className="modal fade" id="myModal" role="dialog">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <button
+                        type="button"
+                        className="close"
+                        data-dismiss="modal"
+                      >
+                        &times;
+                      </button>
+                      <h3>Rate it</h3>
+                    </div>
+                    <div className="modal-body">{this.rateData}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br />
+          </div>
         </div>
       </div>
     );
