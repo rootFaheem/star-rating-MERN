9f73e9699b07a1ec7d26f66b104cf9e1888460b6
Faheem <faheemtech1@gmail.com>
2019-03-10 22:33:25 +0530

    button click to rate it

--- src/App.js
+++ src/App.js
@@ -3,36 +3,63 @@ import "./App.css";
 import Rating from "react-rating";
 
 class App extends Component {
+  state = {
+    show: false
+  };
+
   render() {
+    const show = this.state.show;
+
+    if (show) {
+      this.rateData = (
+        <Rating
+          className="rating"
+          stop={10}
+          emptySymbol={[
+            "fa fa-star-o fa-2x medium",
+            "fa fa-star-o fa-2x medium",
+            "fa fa-star-o fa-2x medium",
+            "fa fa-star-o fa-2x medium",
+            "fa fa-star-o fa-2x medium",
+            "fa fa-star-o fa-2x medium"
+          ]}
+          fullSymbol={[
+            "fa fa-star fa-2x medium",
+            "fa fa-star fa-2x medium",
+            "fa fa-star fa-2x medium",
+            "fa fa-star fa-2x medium",
+            "fa fa-star fa-2x medium",
+            "fa fa-star fa-2x medium"
+          ]}
+          onChange={rate => {
+            console.log(rate);
+            alert(rate);
+          }}
+        />
+      );
+    } else this.rateData = null;
+
     return (
       <div className="App">
-        <h1>hi there</h1>
+        <h1>Let's Rate it !</h1>
         <br />
+
         <div className="ratingbox">
-          <Rating
-            className="rating"
-            stop={10}
-            emptySymbol={[
-              "fa fa-star-o fa-2x medium",
-              "fa fa-star-o fa-2x medium",
-              "fa fa-star-o fa-2x medium",
-              "fa fa-star-o fa-2x medium",
-              "fa fa-star-o fa-2x medium",
-              "fa fa-star-o fa-2x medium"
-            ]}
-            fullSymbol={[
-              "fa fa-star fa-2x medium",
-              "fa fa-star fa-2x medium",
-              "fa fa-star fa-2x medium",
-              "fa fa-star fa-2x medium",
-              "fa fa-star fa-2x medium",
-              "fa fa-star fa-2x medium"
-            ]}
-            onChange={rate => {
-              console.log(rate);
-              alert(rate);
-            }}
-          />
+          <button
+            style={{ marginTop: "40px" }}
+            className="btn btn-success btn-lg"
+            onClick={() => this.setState({ show: true })}
+          >
+            {" "}
+            Rate Now
+          </button>
+          <br />
+
+          {this.rateData}
+          <br />
+          <br />
+          <br />
+          <br />
         </div>
       </div>
     );
